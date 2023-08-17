4085bfaa092a315ee50e6aa7807337f45c76cc17
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-08-17 17:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,33 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+// Function to create a promise that resolves after a random time between min and max seconds
+function createRandomPromise(min, max) {
+  const randomTime = Math.random() * (max - min) * 1000 + min * 1000;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(randomTime);
+    }, randomTime);
+  });
+}
+
+// Array to hold the promises
+ promises = [
+  createRandomPromise(1, 5),
+  createRandomPromise(1, 5),
+  createRandomPromise(1, 5),
+  createRandomPromise(1, 5),
+  createRandomPromise(1, 5)
+];
+
+// Use Promise.any() to wait for the first promise to resolve
+Promise.any(promises)
+  .then((result) => {
+    // Get the output div element
+    const outputDiv = document.getElementById('output');
+    // Display the result in the output div
+    outputDiv.textContent = `Resolved with time: ${result / 1000} seconds`;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
