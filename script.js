4ff7224d694a5f3d53490614c4855e3b7a1bbae8
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-08-23 16:45:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-window.promises = [];
+// script.js
 
-// Do not change the code above this
-// add your promises to the array `promises`
-// Function to create a promise that resolves after a random time between min and max seconds
-function createRandomPromise(min, max) {
-  const randomTime = Math.random() * (max - min) * 1000 + min * 1000;
-  return new Promise((resolve) => {
+// Function to create a promise that resolves after a random time
+function createRandomPromise() {
+  const randomTime = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+  return new Promise(resolve => {
     setTimeout(() => {
-      resolve(randomTime);
+      resolve(`Resolved after ${randomTime} milliseconds`);
     }, randomTime);
   });
 }
 
-// Array to hold the promises
- promises = [
-  createRandomPromise(1, 5),
-  createRandomPromise(1, 5),
-  createRandomPromise(1, 5),
-  createRandomPromise(1, 5),
-  createRandomPromise(1, 5)
+// Create an array of promises
+const promises = [
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise()
 ];
 
-// Use Promise.any() to wait for the first promise to resolve
+// Wait for the first promise to resolve using Promise.any()
 Promise.any(promises)
-  .then((result) => {
-    // Get the output div element
-    const outputDiv = document.getElementById('output');
-    // Display the result in the output div
-    outputDiv.textContent = `Resolved with time: ${result} seconds`;
+  .then(result => {
+    // Print the result to the output div
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = result;
   })
-  .catch((error) => {
-    console.error('Error:', error);
+  .catch(error => {
+    console.error("All promises rejected:", error);
   });
